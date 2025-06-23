8d2092576b5941053e21ba4064aafe6982f85840
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-06-23 12:27:25 -0300

    endpoint

--- src/factura/factura.controller.ts
+++ src/factura/factura.controller.ts
@@ -4,15 +4,27 @@ import { FacturaService } from './factura.service';
 import { Factura } from './factura.entity';
 import { CrearFacturaDto } from './dto/crear-factura.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CrearFacturaConLoginDto } from './dto/crear-factura-login.dto';
+import { AuthService } from 'src/auth/auth.service';
 
 @Controller('facturas')
 export class FacturaController {
-  constructor(private readonly facturaService: FacturaService) {}
+  constructor(private readonly facturaService: FacturaService,private readonly authService: AuthService,) {}
 
-  @UseGuards(JwtAuthGuard)
-  @Post()
-    async crearFactura(@Body() data: CrearFacturaDto): Promise<Factura> {
-    return this.facturaService.crearFactura(data);
+  @Post('login-facturar')
+    async loginYFacturar(@Body() dto: CrearFacturaConLoginDto) {
+    const { access_token } = await this.authService.validarEmpresa(dto.email, dto.password);
+    const empresaId = this.authService.decodeToken(access_token).sub;
+
+    const factura = await this.facturaService.crearFactura({
+        ...dto,
+        empresaId,
+    });
+
+    return {
+        token: access_token,
+        factura,
+    };
     }
 
   @Get()

c8e7240cee013a4bc8511f544bc0ded73c784860
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-06-25 10:10:28 -0300

    bloque catch

--- src/factura/factura.service.ts
+++ src/factura/factura.service.ts
@@ -45,24 +45,34 @@ export class FacturaService {
         }
       );
     } catch (error) {
-      if (error.response?.status === 500) {
-        // Esperar 500 ms y reintentar
-        await new Promise((res) => setTimeout(res, 500));
-        response = await axios.post<FacturaApiResponse>(
-          'https://facturador-production.up.railway.app/facturas',
-          {
-            cuit_emisor: dto.cuit_emisor,
-            importe_total: dto.importe_total,
-            test: dto.test,
-            punto_venta: dto.punto_venta,
-            factura_tipo: dto.factura_tipo,
-            metodo_pago: dto.metodo_pago,
-          }
-        );
-      } else {
-        throw error;
+  const status = error.response?.status;
+  const mensaje = error.response?.data?.message || error.message;
+
+  console.error('Error al facturar:', {
+    status,
+    mensaje,
+    stack: error.stack?.split('\n')[0], // opcional
+  });
+
+  if (status === 500) {
+    // Retry
+    await new Promise((res) => setTimeout(res, 500));
+    response = await axios.post<FacturaApiResponse>(
+      'https://facturador-production.up.railway.app/facturas',
+      {
+        cuit_emisor: dto.cuit_emisor,
+        importe_total: dto.importe_total,
+        test: dto.test,
+        punto_venta: dto.punto_venta,
+        factura_tipo: dto.factura_tipo,
+        metodo_pago: dto.metodo_pago,
       }
-    }
+    );
+  } else {
+    throw new Error(`Error al facturar: ${mensaje}`);
+  }
+}
+
     // ----------------------------
 
     const { cae, vencimiento, nro_comprobante, fecha, qr_url } = response.data;

c28b88b879c0df1e14e28fcc7403dab2af2e6936
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2025-06-27 16:41:17 -0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ if (!global.crypto) {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001','http://localhost:3000','https://jagger.up.railway.app/'], // o true para permitir todos los orígenes (no recomendado en producción)
+    origin: ['http://localhost:3001','http://localhost:3000','https://jagger.up.railway.app'], // o true para permitir todos los orígenes (no recomendado en producción)
     credentials: true, // si usas cookies o autenticación con tokens en headers
   });
   await app.listen(process.env.PORT ?? 3000);
